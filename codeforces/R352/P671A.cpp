47a3a9bede9693c8cc8d0c0f9a770f0587f73bc8
newpolaris <newpolaris@gmail.com>
2016-05-13 00:42:18 +0900

    답안 보고 수정. 1,2,3,4 에서 2,3합이 1,4 보다 클 수 있다는 걸 다시 암

--- codeforces/R352/P671A.cpp
+++ codeforces/R352/P671A.cpp
@@ -1,30 +1,21 @@
-#include <algorithm>
-#include <cmath>
-#include <functional>
-#include <tuple>
-#include <vector>
 #include <iostream>
-#include <fstream>
+#include <algorithm>
+#include <vector>
 #include <string>
-#include <iomanip> 
+#include <functional>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
-vector<pair<int, int>> T;
-
-vector<double> getDist(int ax, int ay)
+inline double dist(double ax, double ay, double bx, double by)
 {
-	vector<double> aa(T.size());
-	for (int i = 0; i < T.size(); i++)
-	{
-		double d = 0;
-		d += (double)(T[i].first - ax)*(T[i].first - ax);
-		d += (double)(T[i].second- ay)*(T[i].second- ay);
-		aa[i] = sqrt(d);
-	}
-	return aa;
+	return sqrt((ax - bx)*(ax - bx) + (ay - by)*(ay - by));
 }
 
+#define DIST(ax, ay, bx, by) 
 int main()
 {
 #if _DEBUG
@@ -38,43 +29,36 @@ int main()
 	in >> ax >> ay >> bx >> by >> tx >> ty;
 	in >> n;
 
-	T = vector<pair<int, int>>(n);
-	for (auto& t : T)
-		in >> t.first >> t.second;
+	vector<double> trashbin(n);
+	vector<pair<double, int>> adil(n);
+	vector<pair<double, int>> bera(n);
 
-	vector<double> dist = getDist(tx, ty);
-	vector<double> aa = getDist(ax, ay);
-	vector<double> bb = getDist(bx, by);
-
-	vector<tuple<double, int, int>> z;
-	for (int i = 0; i < aa.size(); i++)
-		z.push_back(make_tuple(dist[i]-aa[i], i, 0));
-
-	for (int i = 0; i < bb.size(); i++)
-		z.push_back(make_tuple(dist[i]-bb[i], i, 1));
-
-	sort(z.begin(), z.end(), greater<tuple<double, int, int>>());
-	int code = get<2>(z[0]);
-	int sel = get<1>(z[0]);
-	int sec;
-	for (sec = 0; sec < z.size(); sec++)
+	for (int i = 0; i < n; i++)
 	{
-		if (get<2>(z[sec]) != code && get<1>(z[sec]) != sel)
-			break;
+		in >> x >> y;
+		trashbin[i] = dist(tx, ty, x, y);
+		adil[i] = { trashbin[i] - dist(ax, ay, x, y), i };
+		bera[i] = { trashbin[i] - dist(bx, by, x, y), i };
 	}
 
-	double sum = 0;
-	for (int i = 0; i < dist.size(); i++)
-		sum += dist[i]*2;
+	double sum = accumulate(trashbin.begin(), trashbin.end(), 0.0) * 2;
 
-	double d0 = get<0>(z[0]);
-	double d1 = get<0>(z[sec]);
+	sort(adil.begin(), adil.end(), greater<pair<double, int>>());
+	sort(bera.begin(), bera.end(), greater<pair<double, int>>());
 
-	sum -= d0;
-	if (d1 > 0)
-		sum -= d1;
+	double minsum = min(sum-adil[0].first, sum-bera[0].first);
+	if (adil[0].second == bera[0].second)
+	{
+		minsum = min(minsum, sum - adil[0].first - bera[1].first);
+		minsum = min(minsum, sum - adil[1].first - bera[0].first);
+	}
+	else
+	{
+		minsum = min(minsum, sum - adil[0].first - bera[0].first);
+	}
+
 	cout << fixed << setprecision(15);
-	cout << sum << endl;
+	cout << minsum << endl;
+
 	return 0;
 }
-
