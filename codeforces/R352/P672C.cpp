0c7c26d9648404df875cf8fac7e0608b1f358201
newpolaris <newpolaris@gmail.com>
2016-05-14 01:08:17 +0900

    극단적으로 짧게한 코드

--- codeforces/R352/P672C.cpp
+++ codeforces/R352/P672C.cpp
@@ -2,59 +2,40 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
-#include <numeric>
 
 using namespace std;
 
+int n, k;
+long long sum = 0;
+
 int main()
 {
-#if _DEBUG
-	ifstream fin("P672C.in");
-	istream& in = fin;
-#else
 	cin.sync_with_stdio(0);
-	istream& in = cin;
-#endif
-
-	int n, k;
-	in >> n >> k;
-
+	cin >> n >> k;
 	vector<int> A(n);
 	for (auto& a : A)
-		in >> a;
-
+		cin >> a;
 	sort(A.begin(), A.end());
-
-	int mx = A[n - 1];
-	int mn = A[0];
-	int kk = k;
+	int mx = A[n - 1], mn = A[0], kk = k;
 	for (int i = n - 2; i >= 0; --i)
 	{
-		if (A[i] >= mx) // 중간의 값은 넘버 처리를 위해 필요 
+		if (A[i] >= mx) 
 			continue;
 		int e = n - i - 1;
 		int c = min(mx - A[i], kk / e);
 		mx -= c;
 		kk -= c * e;
 	}
-
-	int ko = k;
-	for (int j = 1; j < n; j++)
+	kk = k;
+	for (int i = 1; i < n; i++)
 	{
-		if (A[j] <= mn) // 중간의 값은 넘버 처리를 위해 필요 
+		if (A[i] <= mn) 
 			continue;
-		int c = min(A[j] - mn, ko / j);
+		int c = min(A[i] - mn, kk / i);
 		mn += c;
-		ko -= c * j;
+		kk -= c * i;
 	}
-
-	// int type 이라 accumulator를 사용하면 넘친다
-	typedef long long ll;
-	ll sum = 0;
 	for (int i = 0; i < A.size(); i++)
 		sum += A[i];
-	
 	cout << max(sum % n ? 1 : 0, mx - mn) << endl;
-
-	return 0;
 }
