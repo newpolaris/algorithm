47f9e247282526926f6b487610c8b2a6d48cd972
newpolaris <newpolaris@gmail.com>
2016-05-15 18:57:47 +0900

    TODO: 완전 탐색은 구성함. 거기서 다음 단계로 넘어가지 못함.
    진짜 짜증. 완전 탐색도 좀 구리지만 구성했고
    총 bino 도 구성했는데 다음 단계로 못넘어갔다.
    입력인 n과 m을 가지고 k를 감소시켜서 c++ nextpermu tation 한다는
    생각을 했는데 k를 어떻게 감소시킬지를 DP로만 구성하려고 함.
    답은 bino 였다. 안풀려서 잠시 졸다가 안되서 책보고 짜증나서 대충
    따라침. 근대 Timeover 원인이 멀까?

---
+++ algospot/morse.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <iomanip>
+
+using namespace std;
+
+int C, N, M, K;
+const int B = 1E9+1;
+int bino[201][201];
+
+void calcBino()
+{
+	memset(bino, 0, sizeof(bino));
+	for (int i = 0; i < 201; i++)
+	{
+		bino[i][0] = bino[i][1] = 1;
+		for (int j = 1; j < i; j++)
+		{
+			bino[i][j] = min(B, bino[i-1][j-1] + bino[i-1][j]);
+		}
+	}
+}
+
+void Generate(int n, int m, string s)
+{
+	if (n == 0 && m == 0)
+	{
+		if (--K == 0)
+		{
+			cout << s << endl;
+		}
+		return;
+	}
+
+	if (bino[n+m][n] < K) 
+	{
+		K -= bino[n+m][n];
+		return;
+	}
+
+	if (n > 0) Generate(n-1, m, s + "-");
+	if (m > 0) Generate(n, m - 1, s + "o");
+}
+
+int main()
+{
+#if _DEBUG
+	ifstream fin("morse.in");
+	istream& in = fin;
+#else
+	istream& in = cin;
+#endif
+
+	in >> C;
+
+	calcBino();
+
+	while (C--)
+	{
+		in >> N >> M >> K;
+		Generate(N, M, "");
+	}
+	
+	return 0;
+}
