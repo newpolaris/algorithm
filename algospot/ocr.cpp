98689db3bf78e964b9e57a95802f7fcd659adee8
newpolaris <newpolaris@gmail.com>
2016-05-15 16:13:59 +0900

    통과 3중 배열이 메모리 여유가 의외로 없다. 19K 정도 인것 같은데
    2중 배열로 답 추적하는걸 책에서 보고 수정 후 제출
    freopen 사용

--- algospot/ocr.cpp
+++ algospot/ocr.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-typedef double FP;
+typedef float FP;
 
 int m, q, n;
 vector<vector<FP>> M, T;
@@ -19,9 +19,17 @@ vector<int> S;
 const int MMax = 501;
 const int QMax = 101;
 FP cache[QMax][MMax];
-FP value[QMax][MMax][MMax];
+int choose[QMax][MMax];
 
 int INF = 987654321;
+
+FP GetTP(int step, int i, int prev)
+{
+	FP Tt = (step == 0) ? B[i] : T[prev][i];
+	FP Mt = M[i][S[step]];
+
+	return Tt + Mt;
+}
 
 FP search(int step, int prev)
 {
@@ -35,16 +43,15 @@ FP search(int step, int prev)
 	ret = -INF;
 	for (int i = 0; i < m; i++)
 	{
-		FP Tt = (step == 0) ? B[i] : T[prev][i];
-		FP Mt = M[i][S[step]];
-		FP Tp = Tt + Mt;
-
-		// if (isinf(Tp)) 
-		//		continue;
+		FP Tp = GetTP(step, i, prev);
+		if (Tp < -INF) continue;
 		
-		auto tt = Tp + search(step+1, i);
-		value[step][prev][i] = tt;
-		ret = max(ret, tt);
+		Tp += search(step+1, i);
+		if (Tp > ret)
+		{
+			ret = Tp;
+			choose[step][prev] = i;
+		}
 	}
 
 	return ret;
@@ -52,50 +59,35 @@ FP search(int step, int prev)
 
 void reconstruct(vector<int>& L, int prev, int step)
 {
-	if (step == S.size())
+	if (L.size() == S.size())
 		return;
-
-	int i = 0;
-	FP minV = -INF;
-
-	for (int c = 0; c < m; c++)
-	{
-		auto t = value[step][prev][c];
-		if (t > 0.5) continue;
-		if (minV < t)
-		{
-			i = c;
-			minV = t;
-		}
-	}
-	L.push_back(i);
-	reconstruct(L, i, step+1);
+	int Next = choose[step][prev];
+	L.push_back(Next);
+	reconstruct(L, Next, step+1);
 }
 
 int main()
 {
 #if _DEBUG
-	FILE* pIn = fopen("ocr.in", "r+");
-#else
-	FILE* pIn = stdin;
+	freopen("ocr.in", "r", stdin);
 #endif
 
 	char ss[11];
 	FP dd;
 
-	fscanf(pIn, "%d%d", &m, &q); 
+	scanf("%d%d", &m, &q); 
 
 	vector<string> R(m);
 	for (auto& r : R)
 	{
-		fscanf(pIn, "%s", ss);
+		scanf("%s", ss);
 		r = string(ss);
 	}
 
 	B = vector<FP>(m);
 	for (int c = 0; c < m; c++)
 	{
-		fscanf(pIn, "%lf", &dd);
+		scanf("%f", &dd);
 		B[c] = log10(dd);
 	}
 
@@ -103,7 +95,7 @@ int main()
 	for (int r = 0; r < m; r++)
 	for (int c = 0; c < m; c++)
 	{
-		fscanf(pIn, "%lf", &dd);
+		scanf("%f", &dd);
 		T[r][c] = log10(dd);
 	}
 
@@ -111,7 +103,7 @@ int main()
 	for (int r = 0; r < m; r++)
 	for (int c = 0; c < m; c++)
 	{
-		fscanf(pIn, "%lf", &dd);
+		scanf("%f", &dd);
 		M[r][c] = log10(dd);
 	}
 
@@ -119,25 +111,21 @@ int main()
 	{
 		for (int r = 0; r < MMax; r++)
 		for (int c = 0; c < QMax; c++)
-		{
 			cache[c][r] = 1.0;
-		}
 
-		for (int r2 = 0; r2 < MMax; r2++)
-		for (int r = 0; r < MMax; r++)
-		for (int c = 0; c < QMax; c++)
-			value[c][r2][r] = 1.0;
+		memset(&choose, -1, sizeof(choose));
 
-		fscanf(pIn, "%d", &n);
+		scanf("%d", &n);
 		S = vector<int>(n);
 		for (auto& i : S)
 		{
-			fscanf(pIn, "%s", ss);
+			scanf("%s", ss);
 			string s(ss);
 			auto it = find(R.begin(), R.end(), s);
 			i = distance(R.begin(), it);
 		}
-		FP P = search(0, 0);
+
+		search(0, 0);
 		vector<int> L;
 		reconstruct(L, 0, 0);
 
