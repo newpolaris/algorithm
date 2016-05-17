6c9b3a28c8de5286c59ddaef7a216ca54d07cba6
newpolaris <newpolaris@gmail.com>
2016-05-17 22:18:41 +0900

    수정 후 정상 작동하는 버전

--- baekjoon/2042.cpp
+++ baekjoon/2042.cpp
@@ -7,41 +7,68 @@
 using namespace std;
 
 typedef long long ll;
-
 vector<ll> L;
 int LeafStart;
+int n, m, k;
 
 ll init(int C)
 {
 	if (LeafStart <= C)
 		return L[C];
-
 	L[C] = init(C * 2 + 1) + init(C * 2 + 2);
-
 	return L[C];
 }
 
+
 ll sum(int C, int RS, int RE, int S, int E)
 {
+	if (E > RE || S < RS) return 0LL;
+	if (S > E) return 0LL;
+
 	if (RS == S && RE == E)
 		return L[C];
 
 	int Mid = (RS + RE)/2;
+
 	ll total = 0;
-	if (Mid < S)
-		total += sum(C * 2 + 1, RS, Mid, S, Mid);
-	if (Mid < E)
-		total += sum(C * 2 + 2, Mid+1, RE, Mid+1, E);
+	total += sum(C * 2 + 1, RS, Mid, S, min(E, Mid));
+	total += sum(C * 2 + 2, Mid + 1, RE, max(S, Mid + 1), E);
 	return total;
 }
 
-ll TotalSum(int S, int E)
+ll Sum(int S, int E)
 {
-	return sum(0, 0, L.size() - LeafStart, S, E);
+	return sum(0, 0, LeafStart, S - 1, E - 1);
 }
 
-void update(int L, int V)
+void update(int C, int Level, int T, int V)
 {
+	int TotalLen = LeafStart + 1;
+	int Len = TotalLen / Level;
+	int Count = (C - Level + 1);
+	int RS = Len * Count;
+	int RE = RS + Len - 1;
+
+	if (RS > T) return;
+	if (RE < T) return;
+	if (RS == RE && RS == T)
+	{
+		L[C] = V;
+		return;
+	}
+
+	int LC = C * 2 + 1;
+	int RC = C * 2 + 2;
+
+	update(LC, Level*2, T, V);
+	update(RC, Level*2, T, V);
+
+	L[C] = L[LC] + L[RC];
+}
+
+void Update(int L, int V)
+{
+	update(0, 1, L - 1, V);
 }
 
 int main()
@@ -53,18 +80,18 @@ int main()
 	istream& in = cin;
 #endif
 
-	int n, m, k;
 	in >> n >> m >> k;
-	vector<int> N(n);
+	vector<ll> N(n);
 	for (auto& e : N)
 		in >> e;
 
-	int h = log2(n);
-	int size = pow(2.0, h+1) - 1;
-	LeafStart = pow(2.0, h) + 1;
-
+	int h = ceil(log2(n));
+	int size = pow(2.0, h+1);
+	LeafStart = pow(2.0, h) - 1;
 	L = vector<ll>(size);
 	copy(N.begin(), N.end(), L.begin() + LeafStart);
+
+	init(0);
 
 	m += k;
 	while (m--)
@@ -72,10 +99,10 @@ int main()
 		int a, b, c;
 		in >> a >> b >> c;
 
-		if (a == 0)
-			cout << TotalSum(a, b) << endl;
-		else if (a == 1)
-			update(b, c);
+		if (a == 1)
+			Update(b, c);
+		else if (a == 2)
+			cout << Sum(b, c) << endl;
 	}
+	return 0;
 }
-
