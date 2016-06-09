9802aa5c99e3592c9ea61648d1c2798ae8c71504
newpolaris <newpolaris@gmail.com>
2016-06-10 00:30:43 +0900

    release 16s

--- algospot/blockgame.cpp
+++ algospot/blockgame.cpp
@@ -11,22 +11,25 @@ typedef bitset<25> BS;
 int N = 5;
 const int L = 6;
 
-int cache[1<<25];
-vector<int> Xpos[] = { {0, 0, 1}, {0, 1, 1}, {0, 1, 1}, {0, 0, 1}, {0, 0}, {0, 1} };
-vector<int> Ypos[] = { {0, 1, 1}, {0, 0,-1}, {0, 0, 1}, {0, 1, 0}, {0, 1}, {0, 0} };
+char cache[1<<25];
+vector<char> Xpos[] = { {0, 0, 1}, {0, 1, 1}, {0, 1, 1}, {0, 0, 1}, {0, 0}, {0, 1} };
+vector<char> Ypos[] = { {0, 1, 1}, {0, 0,-1}, {0, 0, 1}, {0, 1, 0}, {0, 1}, {0, 0} };
 
 bool set(BS& bs, int x, int y, int k, bool bSet)
 {
-	vector<int>& xp = Xpos[k], yp = Ypos[k];
+	vector<char>& xp = Xpos[k], yp = Ypos[k];
 
-	for (int i = 0; i < xp.size(); i++)
+	if (bSet)
 	{
-		int xx = x + xp[i], yy = y + yp[i];	
-		if (xx >= N || xx < 0 || yy >= N || yy < 0)
-			return false;
-		int p = yy * N + xx;
-		if (bs[p] == 1) 
-			return false;
+		for (int i = 0; i < xp.size(); i++)
+		{
+			int xx = x + xp[i], yy = y + yp[i];	
+			if (xx >= N || xx < 0 || yy >= N || yy < 0)
+				return false;
+			int p = yy * N + xx;
+			if (bs[p] == 1) 
+				return false;
+		}
 	}
 	for (int i = 0; i < xp.size(); i++)
 	{
@@ -42,6 +45,20 @@ bool set(BS& bs, int x, int y, int k)
 	return set(bs, x, y, k, true);
 }
 
+void print(BS& bs)
+{
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < N; x++)
+		{
+			int pos = y * N + x;
+			cout << bs[pos];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
 void reset(BS& bs, int x, int y, int k)
 {
 	set(bs, x, y, k, false);
@@ -49,14 +66,14 @@ void reset(BS& bs, int x, int y, int k)
 
 bool CanWin(BS& bs)
 {
-	int& ret = cache[bs.to_ulong()];
+	char& ret = cache[bs.to_ulong()];
 	if (ret != -1) return ret;
 
 	ret = 0;
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
 			for (int k = 0; k < L; k++) {
-				if (set(bs, x, y, k)) 
+				if (!set(bs, x, y, k)) 
 					continue;
 				ret = !CanWin(bs);
 				reset(bs, x, y, k);
