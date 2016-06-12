4da62dce2b6798d36955305a3ec450bf7889d1ab
newpolaris <newpolaris@gmail.com>
2016-06-13 00:16:02 +0900

    iterative version

--- algospot/diamondpath.cpp
+++ algospot/diamondpath.cpp
@@ -1,24 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <string.h>
-
-int cache[201][101];
-int diamond[201][101];
-int N;
 
 using namespace std;
-
-int path(int r, int c)
-{
-	if (c < 0 || c >= N || r >= 2*N || diamond[r][c] == 0) return 0;
-	int& ret = cache[r][c];
-	if (ret != 0) return ret;
-
-	ret = diamond[r][c];
-	int cc = (r < N - 1) ? c : c - 1;
-		ret += max(path(r+1, cc), path(r+1, cc+1));
-	return ret;
-}
 
 int main()
 {
@@ -26,18 +9,22 @@ int main()
 	freopen("diamondpath.in", "r", stdin);
 #endif
 
-	int C;
+	int C, N;
 	cin >> C;
 	while (C--) {
 		cin >> N;
-		memset(diamond, 0, sizeof(diamond)); 
+		vector<int> S(N+1);
 		for (int r = 0; r < 2*N-1; r++) {
-			int k = r >= N ? 2*N - 2 - r : r;
-			for (int c = 0; c <= k; c++)
-				cin >> diamond[r][c];
+			vector<int> I(N), T(N+1);
+			int k = r >= N ? 2*N - 1 - r : r + 1;
+			int shift = r >= N ? N - k : 0;
+			for (int c = 0; c < k; c++)
+				cin >> I[c+shift];
+			for (int c = shift; c < k+shift; c++)
+				T[c+1] = I[c] + max(S[c], S[c+1]);
+			S = T;
 		}
-		memset(cache, 0, sizeof(cache));
-		cout << path(0, 0) << endl;
+		cout << S[N] << endl;
 	}
 	return 0;
 }
