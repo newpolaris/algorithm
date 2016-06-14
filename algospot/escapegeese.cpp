f74974f47d4d937a855e8b64596768f80966a450
newpolaris <newpolaris@gmail.com>
2016-06-15 00:17:33 +0900

    각 r 을 돌면서 현재 i 가 더해진 것과 더해지지 않았을 때의 나머지를 저장

--- algospot/escapegeese.cpp
+++ algospot/escapegeese.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
-#include <iostream>
-#include <string.h>
+#include <cstring>
 
 using namespace std;
 
-int N;
-int MOD = 1000000007;
-int cache[501][101][500];
-
-int count(int n, int k, int r)
-{
-	if (n > N) return 0;
-
-	int& ret = cache[n][k][r];
-	if (ret != -1) return ret;
-
-	if (k == 0) return ret = (r == 0);
-
-	return ret = count(n+1, k-1, (r+n)%N) % MOD + count(n+1, k, r) % MOD;
-}
+const int MaxN = 500, MaxK = 100, MOD = 1000000007;
+int cache[2][MaxK+1][MaxN];
 
 int main()
 {
 #ifdef _DEBUG
-	freopen("escapegeese.in", "r", stdin);
+	freopen("escapegeese.in","r",stdin);
 #endif
+	int T, N, K;
 
-	int T, K;
 	cin >> T;
-	while (T--) {
+	while (T--)
+	{
 		cin >> N >> K;
-		memset(cache, -1, sizeof(cache));
-		cout << count(0, K, 0) << endl;
+		memset(cache, 0, sizeof(cache));
+		cache[0][0][0] = cache[0][1][0] = 1;
+		for (int n = 1; n < N; n++) {
+			int PP = (n - 1) % 2, NN = n % 2;
+			memset(cache[NN], 0, sizeof(cache[NN]));
+			for (int k = 0; k <= K; k++) {
+				for (int r = 0; r < N; r++) {
+					int& A = cache[NN][k][r];
+					A = (A + cache[PP][k][r]) % MOD;
+					if (k < K) {
+						int& B = cache[NN][k + 1][(r + n) % N];
+						B = (B + cache[PP][k][r]) % MOD;
+					}
+				}
+			}
+		}
+		cout << cache[(N-1)%2][K][0] << endl;
 	}
+	return 0;
 }
