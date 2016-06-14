361185d0c1f340a809e4b580d6dd3079c50335c0
newpolaris <newpolaris@gmail.com>
2016-06-15 01:09:17 +0900

    cacading 제거. K 가 누적되므로 역으로 계산해야 됨. 주의!
    다른 사람 소스보고 2차원으로 줄임. 문제는 K가 앞에거 보는데,
    방금 계산된 것도 봐서 계속 추가되었다. K 를 역으로 계산하도록 함

--- algospot/escapegeese.cpp
+++ algospot/escapegeese.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 const int MaxN = 500, MaxK = 100, MOD = 1000000007;
-int cache[2][MaxK+1][MaxN];
+int cache[MaxK+1][MaxN];
 
 int main()
 {
@@ -18,18 +18,17 @@ int main()
 	{
 		cin >> N >> K;
 		memset(cache, 0, sizeof(cache));
-		cache[0][0][0] = cache[0][1][0] = 1;
+		cache[0][0] = cache[1][0] = 1;
 		for (int n = 1; n < N; n++) {
-			int PP = (n - 1) % 2, NN = n % 2;
-			for (int k = 0; k <= K; k++) {
+			for (int k = K; k >= 1; k--) {
 				for (int r = 0; r < N; r++) {
-					int a = cache[PP][k][r];
-					int b = (k > 0) ? cache[PP][k-1][(r - n + N) % N] : 0;
-					cache[NN][k][r] = (a + b) % MOD;
+					int& a = cache[k][r];
+					int b = cache[k-1][(r - n + N) % N];
+					a = (a + b) % MOD;
 				}
 			}
 		}
-		cout << cache[(N-1)%2][K][0] << endl;
+		cout << cache[K][0] << endl;
 	}
 	return 0;
 }
