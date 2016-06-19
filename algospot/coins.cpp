f1519d948a85ef13a204f570402777e5aba8150d
newpolaris <newpolaris@gmail.com>
2016-06-19 21:50:23 +0900

    recursive version. 이것도 답봄

--- algospot/coins.cpp
+++ algospot/coins.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int M, C, coin, MOD = 1000000007;
-long long cache[5001];
+int M, C, coin[101], MOD = 1000000007, cache[101][5001];
+
+int coins(int k, int c)
+{
+	if (c == 0) return 1;
+	if (k <= 0 || c < 0) return 0;
+
+	int& ret = cache[k][c];
+	if (ret != 0) return ret;
+
+	return ret = (coins(k - 1, c) + coins(k, c - coin[k-1])) % MOD;
+}
 
 int main()
 {
@@ -19,15 +30,10 @@ int main()
 		cin >> M >> C;
 		memset(cache, 0, sizeof(cache));
 
-		int coin;
 		for (int i = 0; i < C; i++) 
-		{
-			cin >> coin;
-			cache[coin]++;
-			for (int k = 1; k + coin <= M; k++)
-				cache[k+coin] += cache[k];
-		}
-		cout << (cache[M] % MOD) << endl;
+			cin >> coin[i];
+		sort(coin, coin+C);
+		cout << coins(C, M) << endl;
 	}
 
 	return 0;
