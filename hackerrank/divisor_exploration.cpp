89427455ee6ae436f073368c4fac2bac66bb6096
newpolaris <newpolaris@gmail.com>
2016-09-04 22:35:09 +0900

    accept

--- hackerrank/divisor_exploration.cpp
+++ hackerrank/divisor_exploration.cpp
@@ -4,22 +4,48 @@ typedef long long ll;
 
 using namespace std; 
 
+const int N = 200015;
+const int mod = 1e9+7;
+int cache[N];
+
+ll moddiv(ll x, ll y, ll m) {
+	long long ans = 1;
+	while (y > 0) {
+		if (y % 2 != 0) {
+			ans *= x;
+			ans %= m;
+		}
+		x *= x;
+		x %= m;
+		y /= 2;
+	}
+	return ans;
+}
+
 int main()
 {
 #ifdef _DEBUG
 	freopen("divisor_exploration.in", "r", stdin);
 #endif
 	int d, m, a;
+
 	scanf("%d", &d);
+	cache[0] = 0;
+	for (int i = 1; i < N; i++) {
+		ll p = cache[i-1];
+		cache[i] = (p + i) % mod;
+	}
+	for (int i = 2; i < N; i++) {
+		ll p = cache[i-1];
+		ll q = cache[i];
+		cache[i] = (p*q) % mod;
+	}
+	
 	while (d--) {
 		scanf("%d%d", &m, &a);
-		ll mod = 1e9+7;
-		ll c = 1;
-		for (int i = 0; i < m; i++) {
-			c = c * (((1+i+1+a)*(1+i+2+a)/2) % mod);
-			c = c % mod;
-		}
-		int ans = c % mod;
+		ll div2 = moddiv(cache[a+1], mod-2, mod);
+		ll A = cache[a+m+1];
+		int ans = (A * div2) % mod;
 		printf("%d\n", ans);
 	}
 }
