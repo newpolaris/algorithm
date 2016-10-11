4b1c1fe22b2df8b4833b4b911dcbf87900ac676b
newpolaris <newpolaris@gmail.com>
2016-10-11 16:00:08 +0900

    바로 아래의 자손만 고려하는 문제 수정. heapsort 추가

--- heapify.cpp
+++ heapify.cpp
@@ -3,32 +3,72 @@
 #include <algorithm>
 #include <iterator>
 #include <initializer_list>
+#include <random>
 
 using namespace std;
 
-void heapify(vector<int>& v, int i)
+// To heapify a subtree rooted with node i which is
+// ans index in v
+void heapify(vector<int>& v, int n, int i)
 {
 	int left = 2 * i + 1;
 	int right = 2 * i + 2;
+	// Initialize largest as root
 	int largest = i;
 
-	if (left < v.size() && v[largest] < v[left])
+	if (left < n && v[largest] < v[left])
 		largest = left;
 
-	if (right < v.size() && v[largest] < v[right])
+	if (right < n && v[largest] < v[right])
 		largest = right;
 
+	// If largest is not root
 	if (i != largest)
 	{
 		swap(v[i], v[largest]);
-		heapify(v, largest);
+
+		// Recursively heapify the affected sub-tree
+		heapify(v, n, largest);
 	}
+}
+
+// main function to do heap sort
+void heapSort(vector<int>& arr)
+{
+	int n = arr.size();
+
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+ 
+    // One by one extract an element from heap
+    for (int i = n-1; i >= 0; i--)
+    {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+ 
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
 }
 
 int main()
 {
-	vector<int> arr = { 1, 14, 10, 8, 7, 9, 3, 2, 4 };
-	heapify(arr, 0);
+	vector<int> arr = { 13, 8, 10, 14, 7, 9, 3, 2, 4 };
+
+	// Build heap (rearrange array)
+	int n = arr.size();
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+
+	copy(begin(arr), end(arr), ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	random_device rd;
+	mt19937 g(rd());
+	shuffle(arr.begin(), arr.end(), g);
+
+	heapSort(arr);
 
 	copy(begin(arr), end(arr), ostream_iterator<int>(cout, " "));
 
