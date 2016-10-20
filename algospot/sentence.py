5c44b3d68aae6866b1f0460a759088e4ca214ea4
newpolaris <newpolaris@gmail.com>
2016-10-20 17:04:14 +0900

    Accept. fleo0917's source

--- algospot/sentence.py
+++ algospot/sentence.py
@@ -1,12 +1,3 @@
-m, n = map(int, input().split())
-dic = {}
-for i in range(m):
-    line = input()
-    for i in range(1, len(line)+1):
-        dic[line[:i]] = 1
-cnt = 0
-for i in range(n):
-    line = input()
-    if line in dic:
-        cnt += 1
-print(cnt)
+M, N = map(int, input().split())
+index = '\n'.join(input() for _ in range(M))
+print(sum(input() in index for _ in range(N)))
