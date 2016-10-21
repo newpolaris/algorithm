b62439ae8ac0710d095799c7f41d84fef169ccc9
newpolaris <newpolaris@gmail.com>
2016-10-21 15:00:56 +0900

    Max with key, isalpha, count value with sum if

--- algospot/koogle.py
+++ algospot/koogle.py
@@ -1,16 +1,11 @@
 import math
 
 
+def sec(s):
+    cnt = sum(c.isalpha() for c in s)
+    return math.log10(26)*cnt + len(s) - cnt
+
 for c in range(int(input())):
     n = int(input())
-    l = sorted([input().lower() for i in range(n)])
-    pw = ('', 0)
-    for s in l:
-        cnt = 0
-        for c in s:
-            if 'a' <= c <= 'z':
-                cnt += 1
-        power = cnt + math.log(10)*(len(s) - cnt)/math.log(26)
-        if pw[1] < power:
-            pw = (s, power)
-    print(pw[0])
+    pw = sorted([input().lower() for i in range(n)])
+    print(max(pw, key=sec))
