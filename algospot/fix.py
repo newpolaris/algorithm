8ecb5a5a730fa6c575bfd5f46549283533dc9b9e
newpolaris <newpolaris@gmail.com>
2016-10-22 01:32:20 +0900

    enumerate

--- algospot/fix.py
+++ algospot/fix.py
@@ -1,4 +1,3 @@
 for t in range(int(input())):
     n = int(input())
-    l = list(map(int, input().split()))
-    print(sum(t+1 == l[t] for t in range(len(l))))
+    print(sum(i == t for i, t in enumerate(map(int, input().split()), 1)))
