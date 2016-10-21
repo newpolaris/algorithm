b6dd66c1edf9737d5cb1dc3dc30bd5b1246b12d9
newpolaris <newpolaris@gmail.com>
2016-10-21 13:17:15 +0900

    zip is used to read 2d pair input

--- algospot/bookstore.py
+++ algospot/bookstore.py
@@ -1,19 +1,15 @@
+from operator import itemgetter
+
+
+def cost(store):
+    cost, point = 0, 0
+    for c, p in store:
+        cost += c - min(c, point)
+        point += p - min(c, point)
+    return cost
+
 for c in range(int(input())):
     n, m = map(int, input().split())
-    a = [list(map(int, input().split())) for _ in range(n)]
-    d = [[0]*n for _ in range(m)]
-    for s in range(m):
-        for b in range(n):
-            d[s][b] = (a[b][s*2], a[b][s*2+1])
-    for s in range(m):
-        d[s].sort(key=lambda x: (-x[1], x[0]))
-    price = 10000*n
-    for s in range(m):
-        p = 0
-        point = 0
-        for b in range(n):
-            saleprice = max(d[s][b][0] - point, 0)
-            p = p + saleprice
-            point = d[s][b][1] + (point - min(d[s][b][0], point))
-        price = min(price, max(p, 0))
-    print(price)
+    stores = zip(*(zip(*[map(int, input().split())]*2) for _ in range(n)))
+    stores = (sorted(s, key=itemgetter(1), reverse=True) for s in stores)
+    print(min(map(cost, stores)))
