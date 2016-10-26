fefdc46b74c84772732965123ad05227222d694d
newpolaris <newpolaris@gmail.com>
2016-10-26 17:52:57 +0900

    Add comment

--- algospot/fence.cpp
+++ algospot/fence.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
@@ -9,21 +8,27 @@ vector<int> fence;
 
 int solve(int left, int right)
 {
+	// 1개 블록 처리
 	if (left >= right)
 		return fence[left];
 
+	//2개 이상의 블록을 처리
 	int mid = (left+right)/2;
 	int lo = mid, hi = mid+1;
+
+	// 가운데를 기준으로 나눠 처리
 	int ret = max(solve(left, lo), solve(hi, right));
 
 	while (lo >= left || hi <= right)
 	{
+		// target max를 구하자
 		int target = 0;
 		if (lo >= left)
 			target = fence[lo];
 		if (hi <= right)
 			target = max(target, fence[hi]);
 
+		// 보다 큰쪽으로 확장을 시도
 		while (lo >= left && target <= fence[lo])
 			lo--;
 
@@ -39,25 +44,21 @@ int solve(int left, int right)
 int main()
 {
 #if _DEBUG
-	ifstream fin("fence.in");
-	istream& in = fin;
-#else
-	istream& in = cin;
+	freopen("fence.in", "r", stdin);
 #endif
 
 	int C, N;
-	in >> C;
+	cin >> C;
 
 	while (C--)
 	{
-		in >> N;
-		fence = vector<int>(N);
+		cin >> N;
+		fence.resize(N);
 		for (auto& f : fence)
-			in >> f;
+			cin >> f;
 
 		cout << solve(0, N-1) << endl;
 	}
-	
 	return 0;
 }
 
