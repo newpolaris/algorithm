30ec1abb53931894ae8c865d2eac9fa93ef900ab
newpolaris <newpolaris@gmail.com>
2016-10-27 23:40:28 +0900

    Accept

---
+++ algospot/dictionary.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool isCycle;
+
+vector<int> solution;
+vector<bool> used;
+vector<bool> S;
+vector<vector<int>> aj;
+
+void dfs(int v)
+{
+	used[v] = S[v] = 1;
+	for (auto &e : aj[v])
+	{
+		if (!used[e]) dfs(e);
+		else if (S[e] == 1)
+			isCycle = 1;
+	}
+	
+	solution.push_back(v);
+	S[v] = 0;
+}
+
+int main()
+{
+#ifdef _DEBUG
+	freopen("dictionary.in", "r", stdin);
+#endif
+
+	int C, N;
+
+	cin >> C;
+
+	while (C--)
+	{
+		cin >> N;
+
+		solution.clear();
+		isCycle = false;
+		aj = vector<vector<int>>(26, vector<int>());
+
+		vector<string> str(N);
+		for (auto &e : str)
+			cin >> e;
+
+		for (int i = 1; i < N; i++)
+		{
+			string& p = str[i-1];
+			string& c = str[i];
+
+			int Len = min(p.size(), c.size());
+			for (int l = 0; l < Len; l++)
+			{
+				int P = p[l] - 'a';
+				int C = c[l] - 'a';
+
+				if (P != C)
+				{
+					aj[P].push_back(C);
+					break;
+				}
+			}
+		}
+
+		used = vector<bool>(26, false);
+		S = vector<bool>(26, false);
+
+		for (int i = 0; i < 26; i++)
+			if (!used[i]) dfs(i);
+
+		if (isCycle)
+			cout << "INVALID HYPOTHESIS" << endl;
+		else
+		{
+			for (auto e = solution.rbegin(); e != solution.rend(); ++e)
+				cout << (char)(*e + 'a');
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
