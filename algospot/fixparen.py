898ef925cf27c0b7668885249b579397cf75ef2f
newpolaris <newpolaris@gmail.com>
2016-10-27 21:09:42 +0900

    중간에 함수 리턴값을 받아서 처리

--- algospot/fixparen.py
+++ algospot/fixparen.py
@@ -1,18 +1,20 @@
+def correct(l):
+    if not l:
+        return '', ''
+    c = l.pop(0)
+    if c in close:
+        return c, l
+    else:
+        mid, post = correct(l)
+        idx = min(open.index(c), close.index(mid[-1]))
+        mid = mid[:-1] + close[idx]
+        m, p = correct(post)
+        return open[idx] + mid + m, p
+
 for t in range(int(input())):
     s, open = input().split()
     s = list(s)
+    n = len(s)
     dic = {'{': '}', '(': ')', '<': '>', '[': ']'}
     close = [dic[i] for i in open]
-    stack = []
-    for i, c in enumerate(s):
-        if c in open:
-            stack.append(i)
-        else:
-            io = stack.pop()
-            o = s[io]
-            idx = open.index(o)
-            if idx > close.index(c):
-                idx = close.index(c)
-            s[io] = open[idx]
-            s[i] = close[idx]
-    print(''.join(s))
+    print(correct(s)[0])
