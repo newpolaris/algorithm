e6b7eda52a0c642280390138ed9af59e1f1281ad
newpolaris <newpolaris@gmail.com>
2016-10-27 19:50:27 +0900

    Simplify

--- algospot/fixparen.py
+++ algospot/fixparen.py
@@ -1,25 +1,9 @@
-def correct(i, partial):
-    if i >= len(s):
-        return ''
-    c = s[i]
-    if c in open:
-        stack.append(c)
-        return correct(i+1, )
-    else:
-        o = stack.pop()
-        idx = open.index(o)
-        if idx > close.index(c):
-            idx = close.index(c)
-        return open[idx] + partial + close[idx]
-
 for t in range(int(input())):
     s, open = input().split()
     s = list(s)
     dic = {'{': '}', '(': ')', '<': '>', '[': ']'}
     close = [dic[i] for i in open]
     stack = []
-    ans = ''
-    temp = ''
     for i, c in enumerate(s):
         if c in open:
             stack.append(i)
