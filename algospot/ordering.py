964c577e9a3376bc1384feccc3d8b8c66184b6c9
newpolaris <newpolaris@gmail.com>
2016-10-27 14:46:04 +0900

    Topological sort lexi. first order
    heapq and graph degree

--- algospot/ordering.py
+++ algospot/ordering.py
@@ -1,33 +1,25 @@
-import heapq
-
-# f = open("ordering.in")
-# input = f.readline
+from string import ascii_uppercase
+from heapq import heappop, heappush
 
 for c in range(int(input())):
     n, m = map(int, input().split())
-    bRoot = [True] * n
-    bUsed = [False] * n
-    child = [set() for i in range(n)]
-    parent = [set() for i in range(n)]
+    d = {}
+    child = {}
+    for i in ascii_uppercase[:n]:
+        d[i] = 0
+        child[i] = set()
     for i in range(m):
-        dep = input().strip()
-        pa, ch = map(lambda c: ord(c) - ord('A'), dep)
-        child[pa].add(ch)
-        parent[ch].add(pa)
-        bRoot[ch] = False
+        a, b = input().strip()
+        child[a].add(b)
+        d[b] += 1
     h = []
-    root = [i for i in range(len(bRoot)) if bRoot[i]]
-    for i in root:
-        heapq.heappush(h, i)
-    ans = []
-    while len(h) > 0:
-        k = heapq.heappop(h)
-        if bUsed[k]:
-            continue
-
-        if all(bUsed[j] for j in parent[k]):
-            bUsed[k] = True
-            ans.append(k)
-            for i in child[k]:
-                heapq.heappush(h, i)
-    print(''.join([chr(i+ord('A')) for i in ans]))
+    ans = ''
+    [heappush(h, i) for i in d if d[i] == 0]
+    while h:
+        k = heappop(h)
+        ans += k
+        for i in child[k]:
+            d[i] -= 1
+            if d[i] == 0:
+                heappush(h, i)
+    print(ans)
