59aef6d1c9acc6c0e74451952c58f58f46d5e344
newpolaris <newpolaris@gmail.com>
2016-10-28 11:09:47 +0900

    fleo0917's

--- algospot/dictionary.py
+++ algospot/dictionary.py
@@ -1,35 +1,31 @@
-def dfs(i):
-    bUsed[i] = bTwice[i] = True
-    for a in adj[i]:
-        if not bUsed[a]:
-            dfs(a)
-        elif bTwice[a]:
-            bCycle[0] = True
-    sol.append(i)
-    bTwice[i] = False
+# fleo0917's
+from itertools import combinations
+from string import ascii_lowercase as ascii
 
-for c in range(int(input())):
+
+def dfs(now):
+    if visit[now]:
+        return
+    visit[now] = 1
+    for nxt in inv.get(now, []):
+        dfs(nxt)
+    order.append(now)
+
+for case in range(int(input())):
     n = int(input())
-    l = [input().strip() for _ in range(n)]
-    adj = [[] for _ in range(26)]
-    for i in range(1, len(l)):
-        a, b = l[i-1], l[i]
-        k = min(len(a), len(b))
-        for i in range(k):
-            x, y = ord(a[i])-ord('a'), ord(b[i])-ord('a')
-            if x == y:
-                continue
-            adj[y].append(x)
-            break
-    bUsed = [False for i in range(26)]
-    bTwice = [False for i in range(26)]
-    bCycle = [False]
-    sol = []
-    for i in range(26):
-        if not bUsed[i]:
-            dfs(i)
-    sol = [chr(i+ord('a')) for i in sol]
-    if bCycle[0] is True:
+    words = [input().strip() for _ in range(n)]
+    inv = {}
+    for i in range(0, n-1):
+        for j, k in zip(words[i], words[i+1]):
+            x, y = ord(j)-ord('a'), ord(k)-ord('a')
+            if x != y:
+                inv.setdefault(k, set()).add(j)
+                break
+    visit = {c: 0 for c in ascii}
+    order = []
+    for c in ascii:
+        dfs(c)
+    if any(a in inv and b in inv[a] for a, b in combinations(order, 2)):
         print('INVALID HYPOTHESIS')
     else:
-        print(''.join(sol))
+        print(''.join(order))
