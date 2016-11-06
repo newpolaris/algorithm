13ae004e6573394640539db0e3e74f3ed1eee4e3
newpolaris <newpolaris@gmail.com>
2016-11-06 09:51:14 +0900

    Accept

--- algospot/childrenday.py
+++ algospot/childrenday.py
@@ -5,6 +5,7 @@ def bfs():
         q = queue.pop(0)
         for d in digit:
             t = q[0] * 10 + d
+            # 나누는 수가 n 이므로 여기서 체크해도 됨!
             if t >= n:
                 r = t % n + n
             else:
@@ -12,6 +13,7 @@ def bfs():
             num = q[1] * 10 + d
             if r == n+m:
                 return num
+            # 2 layer 구조를 꾸며야 한다. 갔던 노드 또 간다면 comp 대폭 증가임
             if not discovered[r]:
                 discovered[r] = True
                 queue.append((r, num))
@@ -21,7 +23,8 @@ def bfs():
 for c in range(int(input())):
     d, n, m = input().split()
     n, m = map(int, [n, m])
-    digit = list(map(int, list(d)))
+    # 사전순으로 가장 먼저 있는 걸 찾기 위해
+    digit = sorted(list(map(int, list(d))))
     discovered = [False] * (2*n)
     ans = bfs()
     if ans > 0:
