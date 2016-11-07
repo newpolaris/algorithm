01b23306e3adc815d91da862116aee2fba0f2da7
newpolaris <newpolaris@gmail.com>
2016-11-07 16:03:18 +0900

    Accept

--- algospot/hanoi4.cpp
+++ algospot/hanoi4.cpp
@@ -17,6 +17,20 @@ int incr(int a) {
 	else return a-1;
 }
 
+int shiftor(int a, int b, int i) {
+	return a | (b << i*2);
+}
+
+// 2 bit clear and bitwise or
+int shiftset(int a, int b, int i) {
+	return (a & ~(0x3 << i*2)) | (b << i*2);
+}
+
+// 2 bit get
+int shiftget(int a, int i) {
+	return (a >> i*2) & 0x3;
+}
+
 int bfs(int here, int goal) {
 	queue<int> q;
 	q.push(here);
@@ -28,17 +42,15 @@ int bfs(int here, int goal) {
 		q.pop();
 		vector<int> top(4, MAX_N+1);
 		for (int i = 0; i < n; i++) {
-			auto pos = (v >> i*2) & 0x3;
+			auto pos = shiftget(v, i);
 			if (top[pos] > i) {
 				top[pos] = i;
 			}
-		} <F4>
+		}
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				if (top[i] >= top[j]) continue;
-				auto next = v;
-				next &= ~(0x3 << top[i]*2);
-				next |= j << top[i]*2;
+				auto next = shiftset(v, j, top[i]);
 				if (cnt[next] == 0) {
 					cnt[next] = incr(cnt[v]);
 					q.push(next);
@@ -62,19 +74,15 @@ int main() {
 		memset(cnt, 0, sizeof(cnt));
 		cin >> n;
 		int v = 0;
-		for (int i = 0; i < 4; i++) {
+		// 뒤집어서 마지막 목표 상태를 0 으로 해도 되돌록.
+		for (int i = 3; i >= 0; i--) {
 			cin >> k;
 			for (int j = 0; j < k; j++) {
 				cin >> a;
-				a--;
-				v |= i << a*2;
+				v = shiftor(v, i, a-1);
 			}
 		}
-		int goal = 0;
-		for (int i = 0; i < n; i++) {
-			goal |= 0x3 << i*2;
-		}
-		cout << bfs(v, goal) << endl;
+		cout << bfs(v, 0) << endl;
 	}
 
 	return 0;
