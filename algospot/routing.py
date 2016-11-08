ac980f99ec83ceadb3eaa8678fa239c52349c4a5
newpolaris <newpolaris@gmail.com>
2016-11-08 10:17:47 +0900

    Time over

--- algospot/routing.py
+++ algospot/routing.py
@@ -1,28 +1,25 @@
-import sys
 from heapq import heappop, heappush
+from math import log, e
 
-input = sys.stdin.readline
-
-for c in range(int(input())):
+for case in range(int(input())):
     n, m = map(int, input().split())
     adj = {i: [] for i in range(n)}
     for _ in range(m):
         a, b, c = input().split()
         a, b = map(int, [a, b])
-        c = float(c)
+        c = log(float(c))
         adj[a].append((b, c))
         adj[b].append((a, c))
-    inf = float("inf")
-    noise = [inf] * n
-    h = [(1.0, 0)]
-    visited = [False] * n
+    noise = [1e200] * n
+    noise[0] = 0
+    h = [(0.0, 0)]
     while h:
         cost, here = heappop(h)
         if noise[here] < cost:
             continue
         for there, cost2 in adj[here]:
-            cost2 *= cost
-            if noise[there] > cost2:
-                noise[there] = cost2
-                heappush(h, (cost2, there))
-    print("%.10f" % noise[n-1])
+            cost3 = cost2 + cost
+            if noise[there] > cost3:
+                noise[there] = cost3
+                heappush(h, (cost3, there))
+    print("%.10f" % e**noise[-1])
