e421d57946315fde8adc8b8c3db56bd49486d06f
newpolaris <newpolaris@gmail.com>
2016-11-23 01:30:18 +0900

    Accept.

---
+++ algospot/lan.py
@@ -0,0 +1,30 @@
+from itertools import combinations
+
+
+input = raw_input
+
+for cese in range(int(input())):
+    n, m = map(int, input().split())
+    x = list(map(int, input().split()))
+    y = list(map(int, input().split()))
+    adj = [[0]*n for _ in range(n)]
+    for i, j in combinations(range(n), 2):
+        adj[i][j] = adj[j][i] = (x[i]-x[j])**2 + (y[i]-y[j])**2
+    for _ in range(m):
+        a, b = map(int, input().split())
+        adj[a][b] = adj[b][a] = 0
+    minWeight = [987654321]*n
+    minWeight[0] = 0
+    added = [False]*n
+    dist = 0.0
+    for _ in range(n):
+        u = -1
+        for i in range(n):
+            if not added[i] and (u == -1 or minWeight[u] > minWeight[i]):
+                u = i
+        added[u] = True
+        dist += minWeight[u] ** 0.5
+        for i in range(n):
+            if not added[i] and adj[u][i] < minWeight[i]:
+                minWeight[i] = adj[u][i]
+    print(dist)
