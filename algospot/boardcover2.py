1b497262108d33e8d8f763e1ccb000cbdb113c2a
newpolaris <newpolaris@gmail.com>
2016-12-21 10:16:36 +0900

    Accept

--- algospot/boardcover2.py
+++ algospot/boardcover2.py
@@ -1,42 +1,30 @@
 # f = open("boardcover2.in")
 # input = f.readline
-input = raw_input
-
-def rotate(A):
-    B = [[0]*len(A) for _ in range(len(A[0]))]
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            B[j][len(A)-i-1] = A[i][j]
-    return B
+# input = raw_input
 
 
-def generateRotations(block):
-    rots = []
-    for r in range(4):
-        oy, ox = -1, -1
-        rotation = []
-        for i in range(len(block)):
-            for j in range(len(block[i])):
-                if block[i][j] == 1:
-                    if oy == -1:
-                        oy, ox = i, j
-                    rotation.append((i-oy, j-ox))
-        rots.append(rotation)
-        block = rotate(block)
-    unique = []
-    [unique.append(item) for item in rots if item not in unique]
-    return unique, len(unique[0])
+def rotate(block):
+    return zip(*reversed(block))
 
 
-def Set(y, x, rot, add):
+def relativeize(block):
+    basei = basej = -1
+    ret = []
+    for i, row in enumerate(block):
+        for j, cell in enumerate(row):
+            if cell == 1:
+                if basei == -1:
+                    basei, basej = i, j
+                ret.append((i-basei, j-basej))
+    return ret
+
+
+def place_block(i, j, rot, add):
     bSuc = True
-    for t in rot:
-        r, c = t
-        r += y
-        c += x
-        if 0 <= r < R and 0 <= c < C:
-            Board[r][c] += add
-            if Board[r][c] > 1:
+    for di, dj in rot:
+        if 0 <= di+i < R and 0 <= dj+j < C:
+            board[di+i][dj+j] += add
+            if board[di+i][dj+j] > 1:
                 bSuc = False
         else:
             bSuc = False
@@ -44,38 +32,50 @@ def Set(y, x, rot, add):
     return bSuc
 
 
-def search(placed, used):
+def search(placed, blanks):
     global best
-    remain = Blanks - (used + placed*blockSize)
-    if remain//blockSize + placed <= best:
+    if blanks / blocksize + placed <= best:
         return
-    y, x = -1, -1
+
     for i in range(R):
         for j in range(C):
-            if Board[i][j] == 0:
-                y, x = i, j
+            if board[i][j] == 0:
                 break
-        if y != -1:
-            break
-    if y == -1:
+        else:
+            continue
+        break
+    else:
         best = max(best, placed)
         return
-    for rot in rotations:
-        if Set(y, x, rot, 1):
-            search(placed+1, used)
-        Set(y, x, rot, -1)
-    Board[y][x] = 1
-    search(placed, used+1)
-    Board[y][x] = 0
-    return placed
 
+    for rot in rotations:
+        if place_block(i, j, rot, 1):
+            search(placed+1, blanks - blocksize)
+        place_block(i, j, rot, -1)
+
+    board[i][j] = 1
+    search(placed, blanks - 1)
+    board[i][j] = 0
+
+
+def binarize(cell):
+    return 1 if cell == '#' else 0
 
 for case in range(int(input())):
     R, C, BR, BC = map(int, input().split())
-    Board = [[1 if c == '#' else 0 for c in input()] for r in range(R)]
-    Block = [[1 if c == '#' else 0 for c in input()] for r in range(BR)]
-    Blanks = R*C - sum([sum(line) for line in Board])
-    rotations, blockSize = generateRotations(Block)
+    board = [list(map(binarize, input())) for r in range(R)]
+    block = [list(map(binarize, input())) for r in range(BR)]
+
+    rotations = []
+    for r in range(4):
+        rot = relativeize(block)
+        if rot not in rotations:
+            rotations.append(rot)
+        block = list(rotate(block))
+
     best = 0
-    search(0, 0)
+    blanks = R*C - sum(sum(line) for line in board)
+    blocksize = len(rotations[0])
+
+    search(0, blanks)
     print(best)
