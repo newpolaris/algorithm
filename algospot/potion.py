556ffe8beeb52e1aa7c6d6956eb0bad0de3a88db
newpolaris <newpolaris@gmail.com>
2016-12-24 23:51:33 +0900

    Add comment

--- algospot/potion.py
+++ algospot/potion.py
@@ -9,12 +9,17 @@ def gcd(a, b):
 
 for c in range(int(input())):
     n = int(input())
+    # 넣어야할 양
     r = list(map(int, input().split()))
+    # 넣어버린 양
     p = list(map(int, input().split()))
     res = reduce(gcd, r)
+    # 넣어야할 양의 비율 구하기
     frac = [i//res for i in r]
-    # instead of ceil. it use (a-1)//b + 1
-    ratio = max((a-1)//b + 1 for a, b in zip(p, frac))
+    # 비율 중 가장 높은 것에 맞추기 위해 비율 계산
+    # Instead of ceil(), use (a-1)//b + 1
+    ratio = max((a-1)//b+1 for a, b in zip(p, frac))
     ratio = max(ratio, 1)
+    # 가장 높은 비율에 맞춰 계속 넣기
     need = [ratio*a - b for a, b in zip(frac, p)]
     print(' '.join(str(i) for i in need))
