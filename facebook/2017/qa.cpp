a1475b4dc067cf1e9b21fd47505b9ecd9e146e6b
newpolaris <newpolaris@gmail.com>
2017-01-09 16:52:33 +0900

    Simplify

--- facebook/2017/qa.cpp
+++ facebook/2017/qa.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 int main() {
-#ifdef _DEBUG
-	freopen("qa.in", "r", stdin);
-	// freopen("qa.out", "w", stdout);
+	freopen("qa.in2", "r", stdin);
+#ifndef _DEBUG
+	freopen("qa.out2", "w", stdout);
 #endif
 	int T;
 	double P, X, Y;
 	cin >> T;
-	const double pi = acos(0.0)*2;
+	const double twopi = acos(0.0)*4;
 	for (int i = 1; i <= T; i++) { 
 		cin >> P >> X >> Y;
 		X -= 50; Y -= 50;
-		double deg = (atan2(Y, X)/pi*180);
-		double progress = fmod(360-deg+90, 360) / 3.60;
+		double angle = atan2(Y, X)-atan2(50, 0);
+		double progress = fmod(twopi-angle, twopi) / twopi * 100;
 		cout << "Case #" << i << ": " 
 			<< ((X*X+Y*Y <= 50*50 && progress <= P) ? "black" : "white") 
 			<< endl;
