3240a16c2ebc6e7ff07e9f41f46743b7aa026101
newpolaris <newpolaris@gmail.com>
2017-01-21 23:42:47 +0900

    Simple Hash table impl. move function test

--- ds/HashTable.cpp
+++ ds/HashTable.cpp
@@ -2,38 +2,65 @@
 #include <vector>
 #include <list>
 #include <cassert>
-#include <boost/optional.hpp>
+#include <functional>
 
 namespace test {
 	using std::cout;
 	using std::endl;
 	struct Test {
-		Test() {
-			cout << "H" << endl;
+		int k_;
+		Test(int k) : k_(k) {
+			cout << k_ << " C" << endl;
+		}
+		virtual ~Test() {
+			cout << k_ << " D" << endl;
 		}
 		Test(const Test& h) {
-			cout << "H2" << endl;
+			k_ = h.k_;
+			cout << k_ << " COPY" << endl;
 		}
 		Test(Test&& h) {
-			cout << "H3" << endl;
+			std::swap(k_, h.k_);
+			cout << k_ << " MOVE C" << endl;
+		}
+		Test& operator=(Test&& h) {
+			if (this != &h)  
+			{  
+				std::swap(k_, h.k_);
+				cout << k_ << " MOVE A" << endl;
+			}
+			return *this;
 		}
 	};
+	bool operator==(const Test& a, const Test& b) { return a.k_ == b.k_; }
+
 }
 
-// Temp
-using Key=int;
-using Val=int;
+namespace std {
+	template<> struct hash<test::Test> {
+        typedef test::Test argument_type;
+        typedef std::size_t result_type;
+        result_type operator()(argument_type const& s) const {
+            return hash<int>{}(s.k_);
+        }
+	};
+}
 
 namespace hash {
 	using std::list;
 	using std::vector;
-	using boost::optional;
-	using boost::none;
-	const int LoadFactor = 5;
 
+	template <typename Key>
+	int hash(const Key& key) {
+		int h = std::hash<Key>{}(key);
+		return (h ^ (h >> 16));
+	}
+
+	template <typename Key, typename Val>
 	class HashTable {
 	private:
 		using Node = std::pair<Key, Val>;
+		const int LoadFactor = 5;
 		struct HashTableNode {
 			list<Node> list_;
 		};
@@ -42,44 +69,69 @@ namespace hash {
 
 		// if already exist return false
 		// else true
-		bool insert(std::pair<Key, Val>&& p) {
-			if (find(p.first) != none) 
+		bool insert(Node&& p) {
+			if (find(p.first) != nullptr) 
 				return false;
 
-			auto k = Hash(p.first);
+			auto k = index(p.first);
 			auto& chain = table_[k].list_;
-			chain.push_front(p);
-			return false;
+			chain.push_front(std::move(p));
+			count_++;
+
+			if (count_ / table_.size() > LoadFactor)
+				rehash();
+
+			return true;
 		}
 
-		int Hash(const Key& key) {
-			return 0;
+		int index(const Key& key, int size) {
+			return hash(key) % size;
+		}
+		int index(const Key& key) {
+			return index(key, table_.size());
+		}
+
+		void rehash() {
+			decltype(table_) table(table_.size()*2);
+			int count = 0;
+
+			for (auto& c : table_) {
+				for (auto& l : c.list_) {
+					auto k = index(l.first, table.size());
+					auto& chain = table[k].list_;
+					chain.push_front(move(l));
+					count++;
+				}
+			}
+			// std::move is used so, it isn't guarantee table_ is not modified on exception occurs.
+			std::swap(table_, table);
+			std::swap(count_, count);
 		}
 
 	public:
 		HashTable(int size) {
-			table_.resize(size/LoadFactor);
+			table_.resize(1 + size/LoadFactor);
 		}
 
-		// optional test
-		optional<Val&> find(const Key& key) {
-			auto k = Hash(key);
+		Val* find(Key&& key) {
+			return find(key);
+		}
+
+		Val* find(const Key& key) {
+			int k = index(key);
 			auto& chain = table_[k].list_;
 			for (auto& c : chain) {
 				if (c.first == key) {
-					// https://goo.gl/FO9J6i
-					// optional reference binding : portability issue
-					return c.second;
+					return &c.second;
 				}
 			}
-			// null value
-			return none; 
+			return nullptr; 
 		}
 
 
 		// public insert function can handle rvalue-reference
 		bool insert(Key&& key, int data) {
-			return insert(std::make_pair(key, data));
+			return insert(std::make_pair(std::move(key), data));
 		}
 
 		// public insert function can handle const lvalue
@@ -88,7 +140,7 @@ namespace hash {
 		}
 
 		bool remove(const Key& key) {
-			auto k = Hash(key);
+			auto k = index(key);
 			auto& chain = table_[k].list_;
 			for (auto iter = chain.begin();
 				 iter != chain.end();
@@ -104,13 +156,21 @@ namespace hash {
 }
 
 int main() {
+	using Key=test::Test;;
+	using Val=int;
 
-	hash::HashTable h(10);
-	std::vector<int> v(10);
-	h.insert(Key(), 5);
-	auto t = h.find(Key());
-	if (t.is_initialized()) 
-		t.get() = 10;
+	hash::HashTable<test::Test, int> h(1);
+	h.insert(Key(1), 10);
+	h.insert(Key(2), 9);
+	assert(h.find(Key(2)) != nullptr);
+	h.insert(Key(3), 8);
+	h.insert(Key(4), 7);
+	h.insert(Key(5), 6);
+	h.insert(Key(6), 5);
+	assert(h.find(Key(2)) != nullptr);
+	assert(h.find(Key(6)) != nullptr);
+	*h.find(Key(6)) = 1;
+	assert(*h.find(Key(6)) == 1);
 
 	return 0;
 }
