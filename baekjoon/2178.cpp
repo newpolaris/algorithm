f3fcec5a13f69e3949ea3f5c47b79eb99eb3a6e8
newpolaris <newpolaris@gmail.com>
2017-02-20 01:05:10 +0900

    BFS에서 실수하면 망한다는걸 깨닭음. 지난번에도 고생한거 같은데;

---
+++ baekjoon/2178.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+#define forn(i, a, b) for (int i = (a), i##_end_ = (b); i < i##_end_; ++i)
+#define fori(i, a, b) for (int i = (a), i##_end_ = (b); i > i##_end_; --i)
+#define x first
+#define y second 
+
+int main() {
+#ifdef _DEBUG
+	freopen("2178.in", "r", stdin);
+#endif
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int maze[101][101];
+	char tmp[101];
+	forn(i, 0, n) {
+		scanf("%s", tmp);
+		forn(j, 0, m) maze[i][j] = tmp[j] != '0'; 
+	}
+	queue<pair<int,pair<int,int>>> que;
+	que.push({1, {0, 0}});
+	while (!que.empty()) {
+		auto q = que.front();
+		auto l = q.x, x = q.y.x, y = q.y.y;
+		que.pop();
+
+		// NOTE: 이게 없으면 루핑
+		if (!maze[y][x])
+			continue;
+
+		// visit 설정
+		maze[y][x] = 0;
+		if (y == n-1 && x == m-1) {
+			cout << l << endl;
+			break;
+		}
+		vector<pair<int,int>> ds = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+		for (auto d : ds) {
+			auto nx = x + d.x, ny = y + d.y;
+			if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+			if (maze[ny][nx]) {
+				que.push({l+1, {nx, ny}});
+			}
+		}
+	}
+	return 0;
+}
--- baekjoon/2178.py
+++ baekjoon/2178.py
@@ -1,6 +1,22 @@
-n = int(input())
-t = []
-for i in range(n):
-	t.append(int(input()))
-for i in sorted(t):
-	print(i)
+# pq를 쓸표가 없다. BFS 잖아
+n, m = map(int, input().split())
+maze = [input() for i in range(n)]
+visit = [[1]*m for i in range(n)]
+h = [(1, 0, 0)]
+while len(h):
+    l, x, y = h.pop(0)
+    if not visit[y][x]:
+        continue
+    visit[y][x] = 0
+    if y == n-1 and x == m-1:
+        print(l)
+        break
+    ds = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for d in ds:
+        nx = x + d[0]
+        ny = y + d[1]
+        if nx < 0 or nx >= m or ny < 0 or ny >= n:
+            continue
+        if maze[ny][nx] == '0' or visit[ny][nx] == 0:
+            continue
+        h.append((l+1, nx, ny))
