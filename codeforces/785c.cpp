aedea6b8bdac133bb7dc3d7a6d3a280d1bdd872a
newpolaris <newpolaris@gmail.com>
2018-11-04 22:19:07 +0900

    add

--- codeforces/785c.cpp
+++ codeforces/785c.cpp
@@ -1,44 +1,36 @@
 #include <iostream>
 #include <map>
 #include <algorithm>
+#include <cstdint>
 
 #define x first
 #define y second
 
 using namespace std;
-using ll = long long;
+using ll = int64_t;
 
 int main() {
 #ifdef _DEBUG
-	freopen("785c1.in", "r", stdin);
+	freopen("785c0.in", "r", stdin);
 #endif
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	ll n, m;
 	cin >> n >> m;
-	// maximum increase
-	auto nn = min(n, m);
 
-	ll sum = n;
-	ll i; 
-	for (i = nn; i <= 1e18; i++) {
-		sum = min(n, sum+nn);
-		sum -= i;
-		if (sum <= 0) break;
-		if (sum + nn <= n) break;
-	}
 	auto check = [&](ll d) {
-		ll p = (d-nn);
-		return 2*(sum + p*nn) <= d*(d+1)/2 - nn*(nn+1)/2;
+		auto p = d - 1;
+		auto k = n - (p - m + 1)*(m + p)/2 + (p - m + 1)*m;
+		return k - d <= 0;
 	};
-	ll lo = nn, hi = 1e9;
+	ll lo = m, hi = n;
 	while (lo < hi) {
-		auto m = lo + (hi - lo) / 2;
-		if (check(m)) 
-			hi = m;
+		auto k = lo + (hi - lo) / 2;
+		if (check(k)) 
+			hi = k;
 		else
-			lo = m + 1;
+			lo = k + 1;
 	}
 	cout << lo << endl;
 	return 0;
--- codeforces/873b.cpp
+++ codeforces/873b.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main()
 {
 #ifdef _DEBUG
-	freopen("873b0.in", "r", stdin);
+	freopen("873b2.in", "r", stdin);
 #endif
 	int n;
 	cin >> n;
@@ -22,14 +22,14 @@ int main()
 	std::vector<int> balance(n+1);
 	balance[0] = 0;
 	for (int i = 1; i <= n; i++)
-		balance[i] = balance[i-1] + int(s[i] == '0');
+		balance[i] = balance[i-1] + (s[i-1] == '0' ? 1 : -1);
 	const int inf = int(1e7+1);
-	std::vector<int> minIndex(n+1, inf);
+	std::vector<int> minIndex(2*n+1, inf);
 	for (int i = 0; i <= n; i++)
-		minIndex[balance[i]] = min(minIndex[balance[i]], i);
+		minIndex[n+balance[i]] = min(minIndex[n+balance[i]], i);
 	int sol = 0;
 	for (int i = 1; i <= n; i++)
-		sol = max(sol, i - minIndex[balance[i]]);
+		sol = max(sol, i - minIndex[n+balance[i]]);
 	cout << sol << endl;
 	return 0;
 }
